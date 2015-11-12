2e70a02e0c96776abea911f305af1740907bd25b
Lisa <lisaschwetlick@gmail.com>
2015-11-12 12:57:37 +0100

    read text from hash

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
-	var $starwriting = $("#starwriting");
+	var $starwriting = $("#starwriting");	
+	var hash=window.location.hash;
+	if (hash!="") {
+		$starwriting.val(hash.substring(1)); //first character is #
+	}	
 	$starwriting.bind('input propertychange', function () {
 		
-		var text=$starwriting.val();
+		var text=encodeURIComponent($starwriting.val());
 		console.log(text);
-	
+		window.location.hash=text;
+		
+		
+		
 	});
 
 });
